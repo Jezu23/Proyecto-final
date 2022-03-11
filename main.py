742e59650c9c4f087ab4569908ac8dee908a66c3
Jezu23 <101264265+Jezu23@users.noreply.github.com>
2022-03-11 13:07:00 +0100

    creo q esta mal

---
+++ main.py
@@ -0,0 +1,55 @@
+class Bebidas:
+    def __init__(self, agua, cocacola, fanta):
+        self.agua=agua
+        self.cocacola= cocacola
+        self.fanta=fanta
+    
+    def precioAgua(self):
+        print("El precio del %s es 0.25$"% (self.agua))
+        
+    def precioCocacola(self):
+        print("El precio del %s es 1$"% (self.cocacola))
+        
+    def precioFanta(self):
+        print("El precio del %s es 1.25$"% (self.fanta))
+
+class Comida:
+     def __init__(self, bollicao, cheetosPandilla):
+        self.bollicao=bollicao
+        self.cheetosPandilla= cheetosPandilla
+    
+    def precioBollicao(self):
+        print("El precio del %s es 1$"% (self.bollicao))
+        
+    def precioCheetosPandilla(self):
+        print("El precio del %s es 0.45$"% (self.cheetosPandilla))
+
+aguaBezoya = Bebidas("aguaBezoya")
+aguaLanjaron= Bebidas("aguaLanjaron")
+cocacola = Bebidas("cocacola")
+fantaNaranja= Bebidas("fantaNaranja")
+fantaLimon= Bebidas("fantaLimon")
+bollicao
+cheetosPandilla
+        
+ 
+while True:
+    numero = int(input("Introduce un número del 1 al 5: "))
+   
+    
+    if numero == 1:
+        
+    
+    if numero == 2:
+        
+    
+    if numero == 3:
+        
+    
+    if numero == 4:
+        
+    
+    if numero == 5:
+        
+    
+    
