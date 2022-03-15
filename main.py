b3a02f84bb88fa030693cbe57bfc8d40b77c63d8
Jezu23 <101264265+Jezu23@users.noreply.github.com>
2022-03-15 12:52:28 +0100

    15/3/22/1

--- main.py
+++ main.py
@@ -1,55 +1,78 @@
-class Bebidas:
-    def __init__(self, agua, cocacola, fanta):
-        self.agua=agua
-        self.cocacola= cocacola
-        self.fanta=fanta
+class Agua:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
     
-    def precioAgua(self):
-        print("El precio del %s es 0.25$"% (self.agua))
+    def getPrecio(self):
+        print("El precio del %s es %s$" % (self.nombre, self.precio))
         
-    def precioCocacola(self):
-        print("El precio del %s es 1$"% (self.cocacola))
-        
-    def precioFanta(self):
-        print("El precio del %s es 1.25$"% (self.fanta))
-
-class Comida:
-     def __init__(self, bollicao, cheetosPandilla):
-        self.bollicao=bollicao
-        self.cheetosPandilla= cheetosPandilla
+class Cocacola:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
     
-    def precioBollicao(self):
-        print("El precio del %s es 1$"% (self.bollicao))
-        
-    def precioCheetosPandilla(self):
-        print("El precio del %s es 0.45$"% (self.cheetosPandilla))
+    def getPrecio(self):
+        print("El precio de la %s es %s$" % (self.nombre, self.precio))
 
-aguaBezoya = Bebidas("aguaBezoya")
-aguaLanjaron= Bebidas("aguaLanjaron")
-cocacola = Bebidas("cocacola")
-fantaNaranja= Bebidas("fantaNaranja")
-fantaLimon= Bebidas("fantaLimon")
-bollicao
-cheetosPandilla
+class Fanta:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
+    
+    def getPrecio(self):
+        print("El precio de la %s es %s$" % (self.nombre, self.precio))
+
+class Bollicao:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
+    
+    def getPrecio(self):
+        print("El precio del %s es %s$" % (self.nombre, self.precio))
+
+class Cheetos:
+    def __init__(self,nombre, precio):
+        self.nombre=nombre
+        self.precio=precio
+    
+    def getPrecio(self):
+        print("El precio del %s es %s$" % (self.nombre, self.precio))
+
+
+aguaBezoya = Agua("agua Bezoya", 1)
+aguaLanjaron= Agua("agua Lanjaron", 1.20)
+cocacolaNormal = Cocacola("cocacola normal", 1.50)
+fantaNaranja= Fanta("fanta naranja", 1.50)
+fantaLimon= Fanta("fanta limon", 1.50)
+bollicao = Bollicao("bollicao", 1)
+cheetosPandilla= Cheetos("Cheetos Pandilla", 0.45)
         
  
 while True:
-    numero = int(input("Introduce un número del 1 al 5: "))
+    numero = int(input("Introduce un número del 1 al 7: "))
    
-    
     if numero == 1:
-        
+        print(aguaBezoya.getPrecio())
     
     if numero == 2:
-        
+        print(aguaLanjaron.getPrecio())
     
     if numero == 3:
-        
+        print(cocacolaNormal.getPrecio())
     
     if numero == 4:
-        
+        print(fantaNaranja.getPrecio())
     
     if numero == 5:
+        print(fantaLimon.getPrecio())
+    
+    if numero ==6:
+        print(bollicao.getPrecio())
+    
+    if numero ==7:
+        print(cheetosPandilla.getPrecio())
         
     
     
+    
+
